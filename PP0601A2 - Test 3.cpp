61c88c14fab786028a74b29c9bb09c02aabbd6ad
Kamil Gawron <kgawron955@gmail.com>
2019-04-09 15:54:13 +0200

    add test 3

---
+++ PP0601A2 - Test 3.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main() {
+	int liczba;
+	int licznik = 0;
+	vector<int> v;
+	while (cin >> liczba) {
+		v.push_back(liczba);
+	}
+	cout << v[0]<<endl;
+	for (int i = 1; i < v.size(); i++)
+	{
+		if (v[i] == 42 && v[i - 1] != 42) {
+			licznik++;
+		}
+		cout << v[i] << endl;
+		if (licznik == 3){
+			break;
+		}
+	}
+	system("pause");
+	return 0;
+}

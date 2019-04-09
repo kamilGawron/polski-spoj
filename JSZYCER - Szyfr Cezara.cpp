b1e216584892649c7b3d04469896badec8d082d4
Kamil Gawron <kgawron955@gmail.com>
2019-04-09 15:50:09 +0200

    add JSZYCER - Szyfr Cezara

---
+++ JSZYCER - Szyfr Cezara.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <vector>
+#include <string>
+
+
+using namespace std;
+
+int main() {
+	string line;
+	vector <string> v;
+	string alfabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	while (getline(cin,line)) {
+		v.push_back(line);
+	}
+	for (int i = 0; i < v.size(); i++)
+	{
+		cout << v[i] << endl;
+	}
+	system("pause");
+	return 0;
+}
